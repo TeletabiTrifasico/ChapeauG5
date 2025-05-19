04fa600732447747e58b405cba2e94ed381b627e
709662 <158317896+709662@users.noreply.github.com>
2025-05-19 11:54:40 +0200

    Enums Uploaded

---
+++ ChapeauEnums/OrderItemStatus.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChapeauG5.ChapeauEnums
+{
+    public enum OrderItemStatus
+    {
+        Ordered,
+        BeingPrepared,
+        ReadyToBeServed,
+        Served
+    }
+}
---
+++ ChapeauEnums/PaymentMethod.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChapeauG5.ChapeauEnums
+{
+    public enum PaymentMethod
+    {
+        Cash,
+        DebitCard,
+        CreditCard
+    }
+}
