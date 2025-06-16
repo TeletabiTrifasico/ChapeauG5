cad961f2d4a1ab3330b02500b474327e5e060b42
Zeynep Şevval Kırıcı <144331526+zeysevvalk@users.noreply.github.com>
2025-06-16 18:17:04 +0200

    Set as served button done
    Set as served button and list view on occupied table management form are done

--- ChapeauUI/OccupiedTableManagement.Designer.cs
+++ ChapeauUI/OccupiedTableManagement.Designer.cs
@@ -31,11 +31,13 @@
             freeTablebtn = new Button();
             takeOrderbtn = new Button();
             lbltabelNumber = new Label();
+            setAsServedBtn = new Button();
+            lvReadyToBeServedItems = new ListView();
             SuspendLayout();
             // 
             // freeTablebtn
             // 
-            freeTablebtn.Location = new Point(404, 186);
+            freeTablebtn.Location = new Point(404, 126);
             freeTablebtn.Name = "freeTablebtn";
             freeTablebtn.Size = new Size(390, 153);
             freeTablebtn.TabIndex = 0;
@@ -45,7 +47,7 @@
             // 
             // takeOrderbtn
             // 
-            takeOrderbtn.Location = new Point(404, 410);
+            takeOrderbtn.Location = new Point(404, 325);
             takeOrderbtn.Name = "takeOrderbtn";
             takeOrderbtn.Size = new Size(390, 150);
             takeOrderbtn.TabIndex = 1;
@@ -56,17 +58,44 @@
             // lbltabelNumber
             // 
             lbltabelNumber.AutoSize = true;
-            lbltabelNumber.Location = new Point(542, 103);
+            lbltabelNumber.Location = new Point(542, 80);
             lbltabelNumber.Name = "lbltabelNumber";
             lbltabelNumber.Size = new Size(24, 32);
             lbltabelNumber.TabIndex = 2;
             lbltabelNumber.Text = "..";
             // 
+            // setAsServedBtn
+            // 
+            setAsServedBtn.Location = new Point(404, 509);
+            setAsServedBtn.Name = "setAsServedBtn";
+            setAsServedBtn.Size = new Size(390, 142);
+            setAsServedBtn.TabIndex = 3;
+            setAsServedBtn.Text = "Set as Served";
+            setAsServedBtn.UseVisualStyleBackColor = true;
+            setAsServedBtn.Click += setAsServedBtn_Click;
+            // 
+            // lvReadyToBeServedItems
+            // 
+            lvReadyToBeServedItems.Location = new Point(895, 211);
+            lvReadyToBeServedItems.Name = "lvReadyToBeServedItems";
+            lvReadyToBeServedItems.Size = new Size(400, 450);
+            lvReadyToBeServedItems.TabIndex = 4;
+            lvReadyToBeServedItems.UseCompatibleStateImageBehavior = false;
+            lvReadyToBeServedItems.Columns.Clear();
+            lvReadyToBeServedItems.Columns.Add("Name", 150);
+            lvReadyToBeServedItems.Columns.Add("Quantity", 130);
+            lvReadyToBeServedItems.Columns.Add("Status", 100);
+
+            lvReadyToBeServedItems.View = View.Details;
+
+            // 
             // OccupiedTableManagement
             // 
             AutoScaleDimensions = new SizeF(13F, 32F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(1191, 777);
+            ClientSize = new Size(1400, 777);
+            Controls.Add(lvReadyToBeServedItems);
+            Controls.Add(setAsServedBtn);
             Controls.Add(lbltabelNumber);
             Controls.Add(takeOrderbtn);
             Controls.Add(freeTablebtn);
@@ -81,5 +110,7 @@
         private Button freeTablebtn;
         private Button takeOrderbtn;
         private Label lbltabelNumber;
+        private Button setAsServedBtn;
+        private ListView lvReadyToBeServedItems;
     }
 }
