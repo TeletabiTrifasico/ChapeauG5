55e2c75b5d6d4c8e766d8362d57f0da0c0f073e5
Zeynep Şevval Kırıcı <144331526+zeysevvalk@users.noreply.github.com>
2025-06-16 23:43:33 +0200

    Listview columns rearranged
    button sizes and colors are arranged also list view columns

--- ChapeauUI/OccupiedTableManagement.Designer.cs
+++ ChapeauUI/OccupiedTableManagement.Designer.cs
@@ -33,27 +33,30 @@
             lbltabelNumber = new Label();
             setAsServedBtn = new Button();
             lvReadyToBeServedItems = new ListView();
+            label1 = new Label();
             SuspendLayout();
             // 
             // freeTablebtn
             // 
             freeTablebtn.Location = new Point(404, 126);
             freeTablebtn.Name = "freeTablebtn";
-            freeTablebtn.Size = new Size(390, 153);
+            freeTablebtn.Size = new Size(390, 142);
             freeTablebtn.TabIndex = 0;
             freeTablebtn.Text = "Free Table";
             freeTablebtn.UseVisualStyleBackColor = true;
             freeTablebtn.Click += freeTablebtn_Click;
+            freeTablebtn.BackColor = Color.LightGreen;
             // 
             // takeOrderbtn
             // 
             takeOrderbtn.Location = new Point(404, 325);
             takeOrderbtn.Name = "takeOrderbtn";
-            takeOrderbtn.Size = new Size(390, 150);
+            takeOrderbtn.Size = new Size(390, 142);
             takeOrderbtn.TabIndex = 1;
             takeOrderbtn.Text = "Take Order";
             takeOrderbtn.UseVisualStyleBackColor = true;
             takeOrderbtn.Click += takeOrderbtn_Click;
+            takeOrderbtn.BackColor = Color.LightBlue;
             // 
             // lbltabelNumber
             // 
@@ -73,6 +76,7 @@
             setAsServedBtn.Text = "Set as Served";
             setAsServedBtn.UseVisualStyleBackColor = true;
             setAsServedBtn.Click += setAsServedBtn_Click;
+            setAsServedBtn.BackColor = Color.Coral;
             // 
             // lvReadyToBeServedItems
             // 
@@ -81,19 +85,27 @@
             lvReadyToBeServedItems.Size = new Size(400, 450);
             lvReadyToBeServedItems.TabIndex = 4;
             lvReadyToBeServedItems.UseCompatibleStateImageBehavior = false;
-            lvReadyToBeServedItems.Columns.Clear();
-            lvReadyToBeServedItems.Columns.Add("Name", 150);
-            lvReadyToBeServedItems.Columns.Add("Quantity", 130);
-            lvReadyToBeServedItems.Columns.Add("Status", 100);
-
             lvReadyToBeServedItems.View = View.Details;
 
+            lvReadyToBeServedItems.Columns.Add("Name", 150);
+            lvReadyToBeServedItems.Columns.Add("Quantity", 150);
+            lvReadyToBeServedItems.Columns.Add("Status", 150);
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(895, 162);
+            label1.Name = "label1";
+            label1.Size = new Size(288, 32);
+            label1.TabIndex = 5;
+            label1.Text = "Ready to be served items:";
             // 
             // OccupiedTableManagement
             // 
             AutoScaleDimensions = new SizeF(13F, 32F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(1400, 777);
+            Controls.Add(label1);
             Controls.Add(lvReadyToBeServedItems);
             Controls.Add(setAsServedBtn);
             Controls.Add(lbltabelNumber);
@@ -112,5 +124,6 @@
         private Label lbltabelNumber;
         private Button setAsServedBtn;
         private ListView lvReadyToBeServedItems;
+        private Label label1;
     }
 }
